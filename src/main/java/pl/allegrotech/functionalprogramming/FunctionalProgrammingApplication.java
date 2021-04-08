691b1bd948d89cb11dec5097f272b7276fdc4c97
Kacper Czajkowski <czajkowski.kacper99@gmail.com>
2021-04-08 09:21:12 +0200

    edit

--- src/main/java/pl/allegrotech/functionalprogramming/FunctionalProgrammingApplication.java
+++ src/main/java/pl/allegrotech/functionalprogramming/FunctionalProgrammingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FunctionalProgrammingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FunctionalProgrammingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalProgrammingApplication.class, args);
+    }
 
 }
